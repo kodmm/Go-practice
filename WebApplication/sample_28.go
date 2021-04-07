a8fa600fa8030ecb80b194c73b3f715be8209305
kodmm <53547701+kodmm@users.noreply.github.com>
2021-04-07 21:31:57 +0900

    To learn JSON decode && encode

---
+++ WebApplication/sample_28.go
@@ -0,0 +1,31 @@
+package main 
+
+import(
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+func main() {
+	type Person struct {
+		Name string `json:"name"`
+		Age int `json:"age"`
+	}
+
+	p := &Person{Name: "tenntenn", Age: 31}
+
+	var buf bytes.Buffer 
+	enc := json.NewEncoder(&buf)
+	if err := enc.Encode(p); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(buf.String())
+
+	var p2 Person
+	dec := json.NewDecoder(&buf)
+	if err := dec.Decode(&p2); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(p2)
+}
